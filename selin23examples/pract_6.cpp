a976fb275f6d227a9cadb59b020dd0a91b61343e
moonylead1 <ivankozulwork@gmail.com>
2023-11-17 16:50:28 +0200

    pract_6 release

---
+++ selin23examples/pract_6.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+//Зашифрувати уведений з клавіатури текст відповідно кодової таблиці, яка
+//задана як рядковий літерал із послідовності символів, кожний з яких
+//відповідає певній букві алфавіту. Вхідний текст складено тільки із малих
+//букв латинського алфавіту і десяти ци
+
+std::string RandomString(int ch);
+const int ch_MAX = 26;
+
+
+
+int main(int argc, char const *argv[])
+{
+    srand ( time(NULL) ); //to prevent same coding
+    int cypherStrength = 6;
+    char alpha[ch_MAX] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                          'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                          'o', 'p', 'q', 'r', 's', 't', 'u',
+                          'v', 'w', 'x', 'y', 'z' };
+    std::string text;
+    getline(std::cin, text);
+    //std::string a = RandomString(4);
+
+    std::vector <std::string> alphaCoded = {};
+    for (int i = 0; i < ch_MAX; i++){
+        alphaCoded.push_back(RandomString(cypherStrength));
+    }
+    //search element in alphabet and get the code
+    std::string cypher = "";
+    for (int i=0; i < text.size(); i++){
+        for (int j = 0; j < ch_MAX; j++){
+            if (alpha[j] == text[i]){
+                cypher+= alphaCoded[j] + " ";
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < alphaCoded.size(); i++){
+        std::cout << alpha[i] << "\t" <<alphaCoded[i] << "\n";
+    }
+    std::cout << "\n";
+    std::cout << cypher << std::endl;
+
+    return 0;
+}
+
+
+
+std::string RandomString(int ch)
+{
+    char alpha[ch_MAX] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                          'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                          'o', 'p', 'q', 'r', 's', 't', 'u',
+                          'v', 'w', 'x', 'y', 'z' };
+    std::string result = "";
+    for (int i = 0; i<ch; i++)
+        result = result + alpha[rand() % ch_MAX];
+
+    return result;
+}
+
+
